794ce5e4cc219dfbbea21f6562eca036dbb369fd
Divyansh Jha <divyanshj.16@gmail.com>
2018-06-01 00:24:25 +0530

    Add get train data function

--- load_data.py
+++ load_data.py
@@ -36,5 +36,19 @@ def create_vocab(ln,save=False):
 			pickle.dump(idx2word_de,'mappings/idx2word_de')
 		return word2idx_de, idx2word_de
 
+def create_train_data():
+	pass
+
+def create_data(source_sentences, target_sentences):
+	de2idx, idx2de = create_vocab('de')
+	en2idx, idx2en = create_vocab('en')
+
+	x , y, source, tar = [], [] , [], []
+	for sour_sen, tar_sen in zip(source_sentences,target_sentences):
+		x = [de2idx.get(word,1) for word in sour_sen.split()]
+		y = [en2idx.get(word,1) for word in tar_sen.split()]
+
+
+
 if __name__ == '__main__':
 	print(create_vocab('de')[0])
